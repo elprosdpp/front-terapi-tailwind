06bab2497ab8d1a8d5f6cdeb466af427a24dbeee
elprosdpp <sugengpratama45@gmail.com>
2022-11-14 15:44:43 +0700

    add navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="home"></div>
+  <!-- Slider -->
+  <div>
+    <div class="bg-dBlue h-[60rem] opacity-50 w-full absolute" />
+    <img
+      class="w-full h-[60rem] object-cover"
+      src="../assets/img/Fotolia_107626228_M_optimize.jpg"
+      alt="slider"
+    />
+  </div>
+  <!-- End Slider -->
+
+  <!-- Intro Section -->
+  <div class=""></div>
+  <!-- Intro Section -->
 </template>
 
 <script>
